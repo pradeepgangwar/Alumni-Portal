e01264312f9cc31f7c89e0e6476b64b51670c36e
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-04-30 13:34:12 +0530

    modified radio buttons

--- logincheck.php
+++ logincheck.php
@@ -14,7 +14,7 @@ session_start();
 
   <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<script src="https://use.fontawesome.com/994d7c72ea.js"></script>
+  <script src="https://use.fontawesome.com/994d7c72ea.js"></script>
   <script src = "js/bootbox.js"></script>
 <style>
 #bar_blank {
@@ -87,10 +87,21 @@ session_start();
           <input type="radio" name="course" id="btech" value="B. Tech" checked>
           B.Tech
         </label>
+
+         <label>
+          <input type="radio" name="course" id="mtech" value="Dual Degree (B.Tech + M.Tech)">
+          Dual Degree (B.Tech + M.Tech)
+        </label>
       
         <label>
           <input type="radio" name="course" id="mtech" value="M. Tech">
           M.tech
+        </label>
+
+
+        <label>
+          <input type="radio" name="course" id="mtech" value="Dual Degree (M.tech + Ph.D)">
+          Dual Degree (M.tech + Ph.D)
         </label>
 
         <label>
