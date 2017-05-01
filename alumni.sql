79337f5eed8bfa4ad089d982ae175cbf847df0e2
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-05-01 18:43:31 +0530

    Added serial key in sql tables

--- alumni.sql
+++ alumni.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 30, 2017 at 08:44 AM
+-- Generation Time: May 01, 2017 at 03:07 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `alumni` (
+  `SNo` int(20) NOT NULL,
   `enrollment` varchar(20) NOT NULL,
   `name` varchar(60) NOT NULL,
   `date` varchar(30) NOT NULL,
@@ -41,6 +42,25 @@ CREATE TABLE `alumni` (
   `linkedin` varchar(70) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alumni`
+--
+ALTER TABLE `alumni`
+  ADD PRIMARY KEY (`SNo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alumni`
+--
+ALTER TABLE `alumni`
+  MODIFY `SNo` int(20) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
